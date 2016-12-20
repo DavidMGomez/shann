d5adbc2ad63cc18301821656449e31365d317b7e
DavidMGomez <david-gomez12@hotmail.com>
2016-12-20 16:21:34 -0500

    migracion repetida  eliminada

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218184211) do
+ActiveRecord::Schema.define(version: 20161218181303) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
