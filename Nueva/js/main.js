f2304c4a4cabe19450f51697a9d4db06438a15c6
Andres Vidal Zemanate <lenelvidal@unicauca.edu.co>
2016-12-23 21:29:40 -0500

    #1Andres
    Mejorado el Slider
    Corregido el Resposive Design

--- Nueva/js/main.js
+++ Nueva/js/main.js
@@ -1,11 +1,86 @@
 jQuery(function($) {
+    var cont = 0;
+    var arrImagenes = ['mislider.jpg', 'mislider2.jpg', 'mislider3.jpg'];
 
 	$(function(){
 		$('#main-slider.carousel').carousel({
 			interval: 10000,
 			pause: false
 		});
+		
+		var imagenActual = 'fondo-1.jpg';
+		var tiempo = 10000;
+		var id_contenedor = 'main-slider.carousel';
+		var randImage = arrImagenes[cont];
+		console.log(randImage);
+		imagenActual = randImage;
+		cambiarImagenFondo(imagenActual, id_contenedor);
+		setInterval(function () {
+
+		    var randImage = arrImagenes[cont];
+		    console.log(randImage);
+		    imagenActual = randImage;
+		    cambiarImagenFondo(imagenActual, id_contenedor);
+		    cont++;
+
+		    if (cont == arrImagenes.length)
+		    { cont = 0;}
+		}, tiempo)
+		
 	});
+
+
+	
+
+	function cambiarImagenFondo(nuevaImagen, contenedor) {
+	    var contenedor = $('#' + contenedor);
+	    //cargar imagen primero
+	    var tempImagen = new Image();
+	    $(tempImagen).load(function () {
+	       
+	        contenedor.css('backgroundImage', 'url(' + "/Images/dark_tint.png" + ') ,url(' + tempImagen.src + ')');
+            
+	        contenedor.fadeIn(500);
+	    });
+	    tempImagen.src = 'images/' + nuevaImagen;
+	}
+
+
+	$("#previa").click(function (event) {
+	    event.preventDefault();
+	    if (cont == 0) {
+	        cont = arrImagenes.length - 1;
+	    }
+	    else
+	    {
+	        cont--;
+	    }
+	   
+	    var nuevaImagen = arrImagenes[cont];
+
+	    cambiarImagenFondo(nuevaImagen, 'main-slider.carousel');
+	    
+	});
+
+
+	$("#siguiente").click(function (event) {
+	    event.preventDefault();
+	    if (cont == arrImagenes.length-1) {
+	        cont = 0;
+	    }
+	    else {
+	        cont++;
+	    }
+
+	    var nuevaImagen = arrImagenes[cont];
+
+	    cambiarImagenFondo(nuevaImagen, 'main-slider.carousel');
+
+	});
+
+
+
+
 
 	//Ajax contact
 	var form = $('.contact-form');
