9081ad4ad0484e1da905dda8c9b522291d4e49ed
Andres Vidal Zemanate <lenelvidal@unicauca.edu.co>
2017-01-10 15:51:20 -0500

    Search2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'profile/index'
+
   devise_for :users
   root 'welcome#index'
   get 'welcome/publicidad'
   get 'welcome/busquedas'
+
+   get 'profile/index' => 'profile#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
